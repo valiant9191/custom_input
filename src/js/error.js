ac3e9197a224b16d8dfbf6b8874471e13592fae3
Kolko Valentin <valiant9191@gmail.com>
2023-05-15 12:28:39 +0300

    add: error block;

---
+++ src/js/error.js
@@ -0,0 +1,22 @@
+function errorMessage(element, text) {
+    const elemToDelete = document.getElementsByClassName('error__container');
+    setTimeout(() => {
+        for (const el of elemToDelete) {
+            el.remove()
+        };
+    }, 3000)
+
+
+    const errorContainer = document.createElement('div');
+    const errorContent = document.createElement('p');
+
+    errorContainer.className = 'error__container';
+    errorContent.className = 'error__content';
+    errorContent.innerHTML = text;
+
+    errorContainer.appendChild(errorContent);
+    // add to html 
+    element.parentNode.appendChild(errorContainer)
+}
+
+export default errorMessage;
