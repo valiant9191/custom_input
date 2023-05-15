0ed67a94940e5740fc641a2daa24aeda99850294
Kolko Valentin <valiant9191@gmail.com>
2023-05-15 12:29:28 +0300

    add: fetching data from url

---
+++ src/js/fetch.js
@@ -0,0 +1,21 @@
+import validateForm from "./validate.js"
+
+function fetchSubmit() {
+    const form = document.getElementById('form')
+    const data = Object.fromEntries(new FormData(form))
+    const url = "https://jsonplaceholder.typicode.com/posts"
+
+    // check validation 
+    const valid = validateForm(data, form)
+    if (!valid) {
+        return
+    }
+
+    fetch(url, {
+        method: "post",
+        body: JSON.stringify(data)
+    }).then((responce) => responce.json())
+        .then(data => console.log(data))
+}
+
+export default fetchSubmit;
